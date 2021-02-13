5c470b8ac587e3e8b04baca1d1670a93173d2f43
Toyosato-Yamaguchi <gmail877toyosato@gmail.com>
2021-02-14 00:36:28 +0900

    deviseの導入

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname,        presence: true
+  validates :password,          format: {with: /[a-z\d]{6,}/i}
+  validates :last_name,       presence: true
+  validates :first_name,      presence: true
+  validates :last_name_kana,  presence: true
+  validates :first_name_kana, presence: true
+  validates :dirthday,        presence: true
+
+  has_many :products
+  has_many :purchases
+  
+end
