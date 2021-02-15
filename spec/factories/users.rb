2dd3c57ddf7d73cad5c2a9ec08d1493cd6c6862a
Toyosato-Yamaguchi <gmail877toyosato@gmail.com>
2021-02-15 18:56:55 +0900

    モデル単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    person = Gimei.name
+    nickname              {Faker::Name.initials}
+    email                 {Faker::Internet.free_email}
+    password              {"111aaa"}
+    password_confirmation {password}
+    last_name             {person.last.kanji}
+    first_name            {person.first.kanji}
+    last_name_kana        {person.last.katakana}
+    first_name_kana       {person.first.katakana}
+    birthday              {Faker::Date.between(from: '1930-01-01', to:'2021-01-01')}
+  end
+end
