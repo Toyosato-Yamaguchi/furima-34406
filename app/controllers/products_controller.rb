e66840d6d7a770a13fa2d747db46a8cc1313a317
Toyosato-Yamaguchi <gmail877toyosato@gmail.com>
2021-02-16 17:01:50 +0900

    商品の保存

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,10 +3,22 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def product_params
-    params.require(:product).permit(:image).merge(user_id: current_user.id)
+    params.require(:product).permit(:image, :product_name, :product_description, :price, :category_id, :product_state_id, :freight_burden_id, :area_id, :lead_time_id).merge(user_id: current_user.id)
+  end
+
 end
