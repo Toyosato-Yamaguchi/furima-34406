374ba39edf65818fd566a8689f6eae3313fd8da2
Toyosato-Yamaguchi <gmail877toyosato@gmail.com>
2021-02-16 09:58:25 +0900

    ルーティングの修正、userモデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,        presence: true
-  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
-  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
-  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナを使用してください' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナを使用してください' }
-  validates :birthday,        presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name,       format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
+    validates :first_name,      format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
+    validates :last_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナを使用してください' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナを使用してください' }
+    validates :birthday
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
