1e1ea15c0a77434618b6d930301018bb17798cc6
Toyosato-Yamaguchi <gmail877toyosato@gmail.com>
2021-02-16 23:38:37 +0900

    productモデル単体テストコード

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :product do
-    
+    product_name        {Faker::Lorem.word}
+    product_description {Faker::Lorem.sentence}
+    price               {Faker::Number.within(range: 300..9999999)}
+    category_id         {Faker::Number.within(range: 2..10)}
+    product_state_id    {Faker::Number.within(range: 2..7)}
+    freight_burden_id   {Faker::Number.within(range: 2..3)}
+    area_id             {Faker::Number.within(range: 2..48)}
+    lead_time_id        {Faker::Number.within(range: 2..4)}
+
+    association :user
+
+    after(:build) do |product|
+      product.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,90 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品出品機能' do
+    before do
+      @product = FactoryBot.build(:product)
+    end
+
+    context '商品出品できるとき' do
+      it '商品画像、商品名、商品説明、カテゴリー、商品状態、配送料の負担、発送元の地域、発送までの日数、販売価格が必須であること' do
+        expect(@product).to be_valid
+      end
+    end
+    context '商品が出品できないとき' do
+      it '商品画像が空だと出品できない' do
+        @product.image = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が空だと出品できない' do
+        @product.product_name = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Product name can't be blank")
+      end
+      it '商品説明が空だと出品できない' do
+        @product.product_description = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Product description can't be blank")
+      end
+      it 'カテゴリーが空だと出品できない' do
+        @product.category_id = '1'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Category must be other than 1")
+      end
+
+      it '商品状態が空だと出品できない' do
+        @product.product_state_id = '1'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Product state must be other than 1")
+      end
+
+      it '配送料の負担が空だと出品できない' do
+        @product.freight_burden_id = '1'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Freight burden must be other than 1")
+      end
+
+      it '発送元の地域が空だと出品できない' do
+        @product.area_id = '1'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Area must be other than 1")
+      end
+      it '発送までの日数が空だと出品できない' do
+        @product.lead_time_id = '1'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Lead time must be other than 1")
+      end
+
+      it '販売価格が空だと出品できない' do
+        @product.price = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it '販売価格が全角だと出品できない' do
+        @product.price = '１０００'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price is not a number")
+      end
+
+      it '販売価格が300円未満だと出品できない' do
+        @product.price = '299'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price must be greater than 300")
+      end
+
+      it '販売価格が10000000円以上だと出品できない' do
+        @product.price = '10000000'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price must be less than 10000000")
+      end
+
+      it 'ユーザーが紐づいていなければ出品できない' do
+        @product.user = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
