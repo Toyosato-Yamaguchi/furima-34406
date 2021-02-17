1fc47833ef662abf31acbe83011b2acc25791e50
Toyosato-Yamaguchi <gmail877toyosato@gmail.com>
2021-02-17 11:14:28 +0900

    テストコードの修正

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -28,30 +28,30 @@ RSpec.describe Product, type: :model do
         expect(@product.errors.full_messages).to include("Product description can't be blank")
       end
       it 'カテゴリーが空だと出品できない' do
-        @product.category_id = '1'
+        @product.category_id = 1
         @product.valid?
         expect(@product.errors.full_messages).to include("Category must be other than 1")
       end
 
       it '商品状態が空だと出品できない' do
-        @product.product_state_id = '1'
+        @product.product_state_id = 1
         @product.valid?
         expect(@product.errors.full_messages).to include("Product state must be other than 1")
       end
 
       it '配送料の負担が空だと出品できない' do
-        @product.freight_burden_id = '1'
+        @product.freight_burden_id = 1
         @product.valid?
         expect(@product.errors.full_messages).to include("Freight burden must be other than 1")
       end
 
       it '発送元の地域が空だと出品できない' do
-        @product.area_id = '1'
+        @product.area_id = 1
         @product.valid?
         expect(@product.errors.full_messages).to include("Area must be other than 1")
       end
       it '発送までの日数が空だと出品できない' do
-        @product.lead_time_id = '1'
+        @product.lead_time_id = 1
         @product.valid?
         expect(@product.errors.full_messages).to include("Lead time must be other than 1")
       end
@@ -81,13 +81,13 @@ RSpec.describe Product, type: :model do
       end
 
       it '販売価格が300円未満だと出品できない' do
-        @product.price = '299'
+        @product.price = 299
         @product.valid?
         expect(@product.errors.full_messages).to include("Price must be greater than 300")
       end
 
       it '販売価格が10000000円以上だと出品できない' do
-        @product.price = '10000000'
+        @product.price = 10000000
         @product.valid?
         expect(@product.errors.full_messages).to include("Price must be less than 10000000")
       end
