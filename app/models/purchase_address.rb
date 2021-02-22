74150f81906ab20229839a2176428eba788a1690
Toyosato-Yamaguchi <gmail877toyosato@gmail.com>
2021-02-22 14:57:53 +0900

    商品購入機能の実装

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,22 @@
+class PurchaseAddress
+
+  include ActiveModel::Model
+  attr_accessor  :user_id, :product_id, :postal_code, :city, :phone, :block, :building, :region_id, :purchase_id, :token
+  
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :city
+    validates :phone,       format: { with: /\A\d{10,11}\z/ }
+    validates :block
+    validates :user_id
+    validates :product_id
+    validates :purchase_id
+    validates :token
+  end
+  validates :region_id, numericality: { other_than: 1 }
+
+  def save
+    purchase = Purchase.create(user_id: user_id, product_id: product_id)
+    Address.create(postal_code: postal_code, city: city, phone: phone, block: block, building: building, region_id: region_id, purchase_id: purchase.id)
+  end
+end
